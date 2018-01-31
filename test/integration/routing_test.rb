0dc906e61d1d848108da9d2138b3c4a4f3654cc2
Alexander Meindl <a.meindl@alphanodes.com>
2018-01-31 07:03:20 +0100

    #14 fixed routing cleanup bug

--- test/integration/routing_test.rb
+++ test/integration/routing_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class RoutingTest < Redmine::RoutingTest
-  test 'routing sla' do
+  test 'routing messenger' do
     should_route 'GET /projects/1/settings/messenger' => 'projects#settings', id: '1', tab: 'messenger'
     should_route 'PUT /projects/1/messenger_setting' => 'messenger_settings#update', project_id: '1'
   end
