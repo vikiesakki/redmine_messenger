ba65df0ece8cee6167d2526a157c817638827d0d
Alexander Meindl <alex@alphanodes.com>
2018-07-16 13:37:23 +0200

    Fix project patch

--- lib/redmine_messenger/patches/projects_helper_patch.rb
+++ lib/redmine_messenger/patches/projects_helper_patch.rb
@@ -4,8 +4,8 @@ module RedmineMessenger
       def self.included(base)
         base.send(:include, InstanceMethods)
         base.class_eval do
-          alias_method :project_settings_tabs_without_additionals, :project_settings_tabs
-          alias_method :project_settings_tabs, :project_settings_tabs_with_additionals
+          alias_method :project_settings_tabs_without_messenger, :project_settings_tabs
+          alias_method :project_settings_tabs, :project_settings_tabs_with_messenger
         end
       end
 
