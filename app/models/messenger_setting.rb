24095bf9db7142b26dadb30b4222dbd3743bb125
Alexander Meindl <alex@alphanodes.com>
2020-11-14 18:43:09 +0100

    drop validate_url gem

--- app/models/messenger_setting.rb
+++ app/models/messenger_setting.rb
@@ -1,7 +1,7 @@
 class MessengerSetting < ActiveRecord::Base
   belongs_to :project
 
-  validates :messenger_url, url: { allow_blank: true, message: l(:error_messenger_invalid_url) }
+  validates :messenger_url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true }
 
   def self.find_or_create(p_id)
     setting = MessengerSetting.find_by project_id: p_id
